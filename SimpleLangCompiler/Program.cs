71e7b331b38293b52a300a45e73d350d4093909e
StefanHaslhofer <stefan.haslhofer@hitzinger.at>
2026-06-01 11:42:53 +0200

    parse build env from args

--- SimpleLangCompiler/Program.cs
+++ SimpleLangCompiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using SimpleLangCompiler.FrontEnd;
 
-var fileName = args.Length > 0 ? args[0] : "C:\\Users\\haslh\\Documents\\JKU\\14.Semester\\AdvancedCompilerConstruction\\Project\\SimpleLangCompiler\\Tests\\test2.sl";
+var fileName = args.Length > 0 ? args[0] : "C:\\Users\\stefan.haslhofer\\Documents\\JKU\\SimpleLangCompiler\\SimpleLangCompiler\\Tests\\test2.sl";
+var buildEnv = args.Length > 1 ? args[1] : "sim";
 
 Parser parser = new Parser(new Scanner(fileName), buildEnv);
 parser.Parse();
